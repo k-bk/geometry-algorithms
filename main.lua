899f08b35293fc798a51635d3b62afe639dd3997
Karol Bąk <bak.karolek@gmail.com>
2019-11-07 16:13:16 +0100

    Completed Graham and half-Jarvis

--- main.lua
+++ main.lua
@@ -8,31 +8,16 @@ function rand_double(min, max)
    return love.math.random() * (max - min) + min
 end
 
-function my_det(a,b,c, algo)
-   m = { det = function () end }
-   if algo == 1 or algo == 3 then
-      return 
-         a[1]*b[2] + a[2]*c[1] + b[1]*c[2]
-       - a[1]*c[2] - a[2]*b[1] - b[2]*c[1]
-   elseif algo == 2 or algo == 4 then
-      return 
-         ((a[1]-c[1]) * (b[2]-c[2]))
-       - ((a[2]-c[2]) * (b[1]-c[1]))
-   elseif algo == 3 then
-      return m.det({ 
-         { a[1], a[2], 1 },
-         { b[1], b[2], 1 },
-         { c[1], c[2], 1 }})
-   elseif algo == 4 then
-      return m.det({
-         { a[1]-c[1], a[2]-c[2] },
-         { b[1]-c[1], b[2]-c[2] }})
-   end
+function det(a,b,c)
+    return
+      a[1]*b[2] + a[2]*c[1] + b[1]*c[2]
+    - a[1]*c[2] - a[2]*b[1] - b[2]*c[1]
 end
 
-function orient(a,b,c, algo)
-   d = my_det(a,b,c, algo)
-   eps = 1e-10
+function orient(a,b,c)
+   if a == b or b == c or c == a then return 0 end
+   local d = det(a,b,c)
+   local eps = 1e-10
    if d > eps then 
       return 1
    elseif d < -eps then 
@@ -42,106 +27,90 @@ function orient(a,b,c, algo)
    end
 end
 
-
--- MAIN PROGRAM --
-
 function main()
-   local plots = {}
-   local plot = function (p) table.insert(plots, p) end
+   local plot = coroutine.yield
 
-   -- 1.a)
-   local points_a = { color = graph.c.blue }
-   for i = 1,1e5 do
+   local points = { color = graph.c.blue }
+   for i = 1,100 do
       local p = v2(rand_double(-1000, 1000), rand_double(-1000, 1000))
-      table.insert(points_a, p)
+      table.insert(points, p)
    end
-   plot { points_a, title = "Punkty z przedziału  <-1000,1000>" }
+   plot { points, title = "Punkty z przedziału  <-1000,1000>" }
 
-   -- 1.b)
-   local points_b = { color = graph.c.red }
-   for i = 1,1e5 do
-      local p = v2(rand_double(-1e14, 1e14), rand_double(-1e14, 1e14))
-      table.insert(points_b, p)
-   end
-   plot { points_b, title = "Punkty z przedziału  <-1e14, 1e14>" }
+   table.sort(points, function (a,b) return a[1] < b[1] end)
+   table.sort(points, function (a,b) return a[2] < b[2] end)
+   local pivot = points[1]
 
-   -- 1.c)
-   local points_c = { color = graph.c.blue }
-   for i = 1,1e5 do
-      local r = rand_double(0, 2 * math.pi)
-      local p = v2(100 * math.cos(r), 100 * math.sin(r))
-      table.insert(points_c, p)
-   end
-   plot { points_c, title = "Punkty na okręgu o promieniu 100" }
+   ----------------------
+   -- Algorytm Grahama
+   ----------------------
 
-   -- 1.d)
-   local points_d = { color = graph.c.red }
+   table.sort(points, function (a,b) return orient(pivot, a, b) > 0 end)
 
-   local a = v2(-1.0, 0.0)
-   local b = v2(1.0, 0.1)
-   local t = v2(0,0)
-   if math.abs(a[1]-b[1]) > math.abs(a[2]-b[2]) then
-       t_min = (a[1] + 1000) / (a[1] - b[1])
-       t_max = (a[1] - 1000) / (a[1] - b[1])
-   else
-       t_min = (a[2] + 1000) / (a[2] - b[2])
-       t_max = (a[2] - 1000) / (a[2] - b[2])
-   end
+   points.style = "line"
+   plot { points, title = "Posortowane z przedziału  <-1000,1000>" }
+   points.style = "point"
 
-   for i = 1,1e3 do
-      local t = rand_double(t_min, t_max)
-      local p = (1-t)*a + t*b 
-      table.insert(points_d, p)
-   end
-   plot { points_d, title = "Punkty na prostej a[-1,0],  b[1,0.1]" }
-
-   local pp_left, pp_right, pp_on
-   -- 3.a)
-   for i,pp in ipairs{ points_a, points_b, points_c, points_d } do
-      pp_left = { color = graph.c.blue }
-      pp_right = { color = graph.c.red }
-      pp_on = { color = graph.c.green }
-      for _, p in ipairs(pp) do
-         local o = orient(a,b,p, 1)
-         if o == 1 then table.insert(pp_left, p) end
-         if o ==-1 then table.insert(pp_right, p) end
-         if o == 0 then table.insert(pp_on, p) end
+   run = true
+   local p = points
+   local s = { p[1], p[2], p[3], style = "line" }
+   local i = #s + 1 
+   local m = #p
+   while i <= m do
+      if orient(s[#s-1], s[#s], p[i]) > 0 then
+         table.insert(s, p[i])
+         i = i + 1
+      else
+         s[#s] = nil
       end
-      plot { pp_right, pp_left, pp_on, title = "Orientacja "..i }
+      plot { s, p, title = "Algorytm Grahama" }
    end
 
-   print("Summary:")
-   print("  algo", "left", "right", "on")
-   for i,pp in ipairs{ points_a, points_b, points_c, points_d } do
-      print(" graph "..i)
-      for alg = 1,4 do
-         local left, right, on = 0,0,0
-         for _, p in ipairs(pp) do
-            local o = orient(a,b,p, alg)
-            if o == 1 then left = left + 1 end
-            if o ==-1 then right = right + 1 end
-            if o == 0 then on = on + 1 end
+   table.insert(s, s[1])
+   while true do
+      plot { s, p, title = "Algorytm Grahama, koniec" }
+   end
+
+   ----------------------
+   -- Algorytm Jarvisa
+   ----------------------
+
+   local start = p[1]
+   local s = { v2(0,p[1][2]), p[1], style = "line" }
+   repeat
+      local min_i = 1
+      local min_angle = math.huge
+      for k = 2,#p do
+         angle = det(s[#s-1], s[#s], p[k])
+         if angle < min_angle then
+            min_angle = angle
+            min_i = k
          end
-         print("  "..alg, left, right, on)
       end
-   end
-
-   print("\nAlgorithms:")
-   print(" 1. determinant 3x3, my implementation")
-   print(" 2. determinant 2x2, my implementation")
-   print(" 3. determinant 3x3, library function")
-   print(" 4. determinant 2x2, library function")
+      table.insert(s, p[min_i])
+      i = min_i
+      plot { s, p, title = "Algorytm Jarvisa" }
+   until i == 1
 
    while true do
-      for _,p in ipairs(plots) do
-         coroutine.yield(p)
-      end
+      plot { s, p, title = "Algorytm Jarvisa" }
    end
 end
 
 function love.load()
+   run = false
+   stoper = 0
+   step = 0.1
    get_content = coroutine.wrap(main)
    content = get_content() 
+end
+
+function love.update(dt)
+   stoper = stoper + dt
+   if run and stoper > step then
+      stoper = stoper - step 
+      content = get_content()
+   end
 end
 
 function love.mousepressed(x, y, button)
