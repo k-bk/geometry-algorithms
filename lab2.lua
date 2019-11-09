77635af4038e3cb537492ef0ddc59cc452599369
Karol Bąk <bak.karolek@gmail.com>
2019-11-09 23:41:44 +0100

    Change pivot to low-left in Graham, elminate collinear points

--- lab2.lua
+++ lab2.lua
@@ -1,6 +1,24 @@
+function points_on_line(a, b, points)
+   -- 1.d)
+   local points = points or {}
+   for i = 1,10 do
+      local t = love.math.random() 
+      local p = (1-t)*a + t*b 
+      table.insert(points, p)
+   end
+   return points
+end
+
 function lab2()
    local plot = function (x) autorun = false; coroutine.yield(x) end
    local autoplot = function (x) autorun = true; coroutine.yield(x) end
+
+   local rectangle = { color = graph.c.blue }
+   points_on_line(v2(0,0), v2(100,0), rectangle)
+   points_on_line(v2(100,0), v2(100,50), rectangle)
+   points_on_line(v2(100,50), v2(0,50), rectangle)
+   points_on_line(v2(0,50), v2(0,0), rectangle)
+   plot { rectangle, title = "Losowe punkty na prostokącie" }
 
    local points = { color = graph.c.blue }
    for i = 1,100 do
@@ -9,9 +27,19 @@ function lab2()
    end
    plot { points, title = "Punkty z przedziału  <-1000,1000>" }
 
-   table.sort(points, function (a,b) return a[1] < b[1] end)
+   points = rectangle
    table.sort(points, function (a,b) return a[2] < b[2] end)
-   local pivot = points[1]
+   local min = points[1]
+   for _,p in ipairs(points) do
+      if p[2] ~= points[1][2] then
+         break
+      end
+      if p[1] < min[1] then
+         min = p
+      end
+   end
+
+   local pivot = min
 
    ----------------------
    -- Algorytm Grahama 
@@ -30,15 +58,18 @@ function lab2()
    local i = #s + 1 
    local m = #p
    while i <= m do
-      if orient(s[#s-1], s[#s], p[i]) > 0 then
+      o = orient(s[#s-1], s[#s], p[i])
+      if o > 0 then
          table.insert(s, p[i])
+         i = i + 1
+      elseif o == 0 then
+         s[#s] = p[i]
          i = i + 1
       else
          s[#s] = nil
       end
       autoplot { s, p, title = "Algorytm Grahama" }
    end
-
    table.insert(s, s[1])
 
    local hull = { unpack(s) }
@@ -50,6 +81,7 @@ function lab2()
    -- Algorytm Jarvisa
    ----------------------
 
+   local eps = 1e-5
    local pivot = points[1]
    local s = { v2(pivot[1] - 50,pivot[2]), pivot, 
       style = "line", color = graph.c.green }
@@ -69,6 +101,9 @@ function lab2()
          end
       end
 
+      if min_angle < eps and min_angle > -eps then
+         table.remove(s, #s)
+      end
       table.insert(s, min_p)
       autoplot { s, points, title = "Algorytm Jarvisa" }
    until min_p == pivot 
