f32a2a63543ea31adb835eaf32de812aae69815a
Karol Bąk <bak.karolek@gmail.com>
2019-11-18 12:05:45 +0100

    Replace insert calls

--- lab3.lua
+++ lab3.lua
@@ -2,26 +2,25 @@ local lab = {}
 
 function sweep(segments)
 
-   local T = {}
-   local Q = { unpack(segments) }
-   table.sort(Q, function(a,b) return a[1] < b[1] end)
+   local T = btree()
+   local Q = btree()
 
    while #events > 0 do
       if segment_start then
          a,b = neighbours(T, s)
          if a and crossing(a, s) then
-            insert(Q, crossing(a,s))
+            Q:insert(crossing(a,s))
          end
          if b and crossing(b, s) then
-            insert(Q, crossing(b,s))
+            Q:insert(crossing(b,s))
          end
       end
 
       if segment_end then
-         remove(T, s)
+         T:remove(s)
          a,b = neighbours(T, s)
          if a and b and crossing(a, b) then
-            insert(Q, crossing(a,b))
+            Q:insert(crossing(a,b))
          end
       end
 
